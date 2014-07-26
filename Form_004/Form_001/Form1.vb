4c682bbc0a6a489bd53ca971cda2a7490987a31d
HobisJung <hobisjung@gmail.com>
2014-07-26 13:09:34 +0900

    2014-07-26-2

--- Form_004/Form_001/Form1.vb
+++ Form_004/Form_001/Form1.vb
@@ -1,10 +1,8 @@
-﻿Imports System.Xml
-
-'---------------------------------------------------------------------------------------------------------------------------
+﻿'---------------------------------------------------------------------------------------------------------------------------
 ' # 본 스크립트는 닷넷 윈폼을 이용하여 윈도우창과 HTML/JS 또는 플래시와 연동는 샘플을 제공합니다
 ' @Name: VisualBasic.NET WinForm
 ' @Author: HobisJung
-' @Date: 2014-06-25
+' @Date: 2014-07-26
 '---------------------------------------------------------------------------------------------------------------------------
 Public NotInheritable Class Form1
 
@@ -18,9 +16,30 @@ Public NotInheritable Class Form1
         Me.p_InitOnce()
     End Sub
 
+    ' # 초기화 한번
+    Private Sub p_InitOnce()
+        Dim t_s As Size = Me.Size
+        't_s.Width = 1170
+        't_s.Height = 796
+        t_s.Width = 1024
+        t_s.Height = 768
+        Me.ClientSize = t_s
+        Me.MinimumSize = Me.Size
+        Me.Text = "Main"
+
+        Me.WebBrowser1.ObjectForScripting = Me
+        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
+        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        Me.WebBrowser1.ScrollBarsEnabled = False
+
+        Dim t_name As String = Path.GetFileNameWithoutExtension(Application.ExecutablePath)
+        Dim t_src As String = Path.Combine(Environment.CurrentDirectory, t_name & ".html")
+        Me.WebBrowser1.Navigate(t_src)
+    End Sub
+
     ' -
     Private _bFirst As Boolean = True
-
     ' ::
     Protected Overrides Sub SetVisibleCore(ByVal value As Boolean)
         If (Me._bFirst) Then
@@ -63,23 +82,35 @@ Public NotInheritable Class Form1
     Private Sub p_WebBrowser1_PreviewKeyDown(ByVal sender As Object, ByVal pkdea As PreviewKeyDownEventArgs) _
         Handles WebBrowser1.PreviewKeyDown
         '
-        If pkdea.KeyCode = Keys.Escape Then
+        If pkdea.KeyCode.Equals(Keys.Escape) Then
             Me.p_SetFullScreen(False)
+        ElseIf pkdea.KeyCode.Equals(Keys.F5) Then
+            'Me.WebBrowser1.Refresh()
+            Me.p_Js_Call("p_reload", Nothing)
         End If
     End Sub
 
+    '
+    Private _tempSize As Size
     ' :: 풀스크린 설정
     Private Sub p_SetFullScreen(ByVal b As Boolean)
         If b Then
-            Me.TopMost = True
-            Me.FormBorderStyle = FormBorderStyle.None
-            Me.WindowState = FormWindowState.Maximized
-            'Me.Bounds = Screen.PrimaryScreen.Bounds
-            Me.WebBrowser1.Focus()
+            If (Me.TopMost.Equals(False)) Then
+                Me._tempSize = Me.Size
+                Me.FormBorderStyle = FormBorderStyle.None
+                Me.WindowState = FormWindowState.Maximized
+                'Me.Bounds = Screen.PrimaryScreen.Bounds
+                Me.TopMost = True
+                Me.WebBrowser1.Focus()
+            End If
         Else
-            Me.TopMost = False
-            Me.WindowState = FormWindowState.Normal
-            Me.FormBorderStyle = FormBorderStyle.Sizable
+            If (Me.TopMost.Equals(True)) Then
+                Me.WindowState = FormWindowState.Normal
+                Me.FormBorderStyle = FormBorderStyle.Sizable
+                Me.Size = Me._tempSize
+                Me._tempSize = Nothing
+                Me.TopMost = False
+            End If
         End If
     End Sub
 
@@ -113,6 +144,9 @@ Public NotInheritable Class Form1
 
     ' - 윈도우 리사이즈
     Private Const Win_Resize As String = "Win_Resize"
+
+    ' - 윈도우 클라이언트 리사이즈
+    Private Const Win_Min_Resize As String = "Win_Min_Resize"
 
     ' - 윈도우 위치 변경
     Private Const Win_Location As String = "Win_Location"
@@ -163,15 +197,22 @@ Public NotInheritable Class Form1
 
             Case Win_Resize
                 Me.WindowState = FormWindowState.Normal
-
                 Dim t_s As Size = Me.Size
                 t_s.Width = CType(args(0), Integer)
                 t_s.Height = CType(args(1), Integer)
                 Me.Size = t_s
 
+            Case Win_Min_Resize
+                Me.WindowState = FormWindowState.Normal
+                Dim t_s As Size = Me.Size
+                t_s.Width = CType(args(0), Integer)
+                t_s.Height = CType(args(1), Integer)
+                Me.MinimumSize = Me.DefaultMaximumSize
+                Me.ClientSize = t_s
+                Me.MinimumSize = Me.Size
+
             Case Win_Location
                 Me.WindowState = FormWindowState.Normal
-
                 Dim t_p As Point = Me.Location
                 t_p.X = CType(args(0), Integer)
                 t_p.Y = CType(args(1), Integer)
@@ -181,66 +222,12 @@ Public NotInheritable Class Form1
                 Dim t_b As Boolean = CType(args(0), Boolean)
                 p_SetFullScreen(t_b)
 
-
             Case Win_Open
                 Dim t_path As String = CType(args(0), String)
                 'MsgBox(t_path)
                 Process.Start(t_path)
 
         End Select
-    End Sub
-
-    ' # 초기화 한번
-    Private Sub p_InitOnce()
-        Dim t_dllFile As String = "RuntimeSupport.dll"
-        Dim t_dllFilePath As String = Path.Combine(Environment.CurrentDirectory, t_dllFile)
-
-        If (File.Exists(t_dllFilePath)) Then
-            Me.p_WinInit()
-        Else
-            MessageBox.Show(t_dllFile & " 파일을 찾을수 없습니다.", "Main")
-
-            Environment.Exit(1)
-        End If
-    End Sub
-
-    ' #
-    Private Sub p_WinInit()
-        'Me.AutoSizeMode = AutoSizeMode.GrowAndShrink
-        'Me.MaximizeBox = False
-        'Me.StartPosition = FormStartPosition.Manual
-        'Me.Location = New Point(10, 10)
-        'Me.Width = 600
-        'Me.Height = 700
-        'Me.p_SetFullScreen(True)
-        Dim t_s As Size = New Size()
-        t_s.Width = 1170
-        t_s.Height = 796
-        Me.ClientSize = t_s
-        Me.MinimumSize = Me.Size
-        Me.Text = "Main"
-
-
-        Me.WebBrowser1.ObjectForScripting = Me
-        Me.WebBrowser1.IsWebBrowserContextMenuEnabled = False
-        Me.WebBrowser1.AllowWebBrowserDrop = False
-        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
-        Me.WebBrowser1.ScrollBarsEnabled = False
-
-        Dim t_xmlFile As String = _
-                    Path.GetFileNameWithoutExtension(Application.ExecutablePath)
-        t_xmlFile = _
-            Path.Combine(Environment.CurrentDirectory, t_xmlFile & ".xml")
-
-
-        Dim t_name As String = Path.GetFileNameWithoutExtension(Application.ExecutablePath)
-        Dim t_src As String = Path.Combine(Environment.CurrentDirectory, t_name & ".html")
-        Me.WebBrowser1.Navigate(t_src)
-
-        'Me.BringToFront()
-        'Me.Focus()
-        'Me.KeyPreview = True
-        'Me.WebBrowser1.Visible = False
     End Sub
 
 End Class
